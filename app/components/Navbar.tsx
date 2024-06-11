75d7838103e8e2f331f89dff8a09444d68a38030
David Lee <Dai180293@gmail.com>
2024-06-11 12:30:10 +0100

    added menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,31 +1,105 @@
+"use client";
+
 import { Separator } from "@/components/ui/separator";
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
     SheetFooter,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import {
+    HamburgerMenuIcon,
+    ReaderIcon,
+    CalendarIcon,
+    CheckboxIcon,
+    GearIcon,
+    PersonIcon,
+} from "@radix-ui/react-icons";
+import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
+    const pathname = usePathname();
+
+    const changeMenuStatus = () => {
+        setMenuStatus(!menuStatus);
+    };
+
     return (
-        <nav className="flex justify-between items-center p-4">
-            <Sheet>
+        <nav className="flex justify-between items-center p-4 border-b-2 border-slate-100">
+            <Sheet open={menuStatus} onOpenChange={setMenuStatus}>
                 <SheetTrigger>
                     <HamburgerMenuIcon className="size-6" />
                 </SheetTrigger>
-                <SheetContent side="left" className="flex flex-col justify-start items-start">
+                <SheetContent
+                    side="left"
+                    className="flex flex-col justify-start items-start py-6 px-3 sm:max-w-[18rem]"
+                >
                     <SheetHeader>
-                        <SheetTitle className="font-bold text-lg">Scribblepad</SheetTitle>
-                        <SheetDescription>This is where the menu list will go</SheetDescription>
+                        <SheetTitle className="font-bold text-md pl-3">
+                            <Link href="/" onClick={changeMenuStatus}>
+                                Scribblepad
+                            </Link>
+                        </SheetTitle>
                     </SheetHeader>
+                    <section className="flex flex-col justify-between items-start flex-1 w-full mt-10 capitalize">
+                        <ul id="menuList">
+                            <li>
+                                <Link
+                                    href="/notes"
+                                    onClick={changeMenuStatus}
+                                    className={`link ${pathname === "/notes" ? "active" : ""}`}
+                                >
+                                    <span>
+                                        <ReaderIcon />
+                                    </span>
+                                    notes
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/tasks" onClick={changeMenuStatus}>
+                                    <span>
+                                        <CheckboxIcon />
+                                    </span>
+                                    tasks
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/calendar" onClick={changeMenuStatus}>
+                                    <span>
+                                        <CalendarIcon />
+                                    </span>
+                                    calendar
+                                </Link>
+                            </li>
+                        </ul>
+                    </section>
                     <Separator />
-                    <section className="flex-1 w-full">menu list</section>
-                    <Separator />
-                    <SheetFooter>This is where the settings and logout will go</SheetFooter>
+                    <SheetFooter className="w-full">
+                        <ul id="menuList">
+                            <li>
+                                <Link href="/support" onClick={changeMenuStatus}>
+                                    <span>
+                                        <PersonIcon />
+                                    </span>
+                                    support
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/settings" onClick={changeMenuStatus}>
+                                    <span>
+                                        <GearIcon />
+                                    </span>
+                                    settings
+                                </Link>
+                            </li>
+                        </ul>
+                    </SheetFooter>
                 </SheetContent>
             </Sheet>
             <p>other nav things</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,49 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+    ],
+    prefix: "",
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        extend: {
+            fontSize: {
+                sm: "var(--font-size-sm)",
+                base: "var(--font-size-base)",
+                md: "var(--font-size-md)",
+                lg: "var(--font-size-lg)",
+                xl: "var(--font-size-xl)",
+                "2xl": "var(--font-size-xxl)",
+                "3xl": "var(--font-size-xxxl)",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
         },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+    plugins: [require("tailwindcss-animate")],
+} satisfies Config;
 
-export default config
+export default config;
