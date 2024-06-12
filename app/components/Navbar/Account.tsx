68fa51687329a377d6f3184815a5967754166764
David Lee <Dai180293@gmail.com>
2024-06-12 22:05:56 +0100

    added icons to each item

--- app/components/Navbar/Account.tsx
+++ app/components/Navbar/Account.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -7,7 +8,7 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import Link from "next/link";
+import { GearIcon, LockClosedIcon, TrashIcon } from "@radix-ui/react-icons";
 
 export default function Account() {
     return (
@@ -16,13 +17,20 @@ export default function Account() {
                 <Button variant="outline">Account</Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-40">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuLabel>My account</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem className="dropdownItem">
-                    <Link href="/settings">Settings</Link>
+                    <Link href="/settings">
+                        Settings <GearIcon />
+                    </Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem className="dropdownItem">Logout</DropdownMenuItem>
-                <DropdownMenuItem className="dropdownItem">Delete Account</DropdownMenuItem>
+                <DropdownMenuItem className="dropdownItem">
+                    Logout
+                    <LockClosedIcon />
+                </DropdownMenuItem>
+                <DropdownMenuItem className="dropdownItem">
+                    Delete Account <TrashIcon />
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     );
