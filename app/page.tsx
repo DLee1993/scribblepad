01d49219cf5dfdb811c916f01311a5117f333280
David Lee <Dai180293@gmail.com>
2024-06-15 20:31:48 +0100

    added onboarding form component to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import OnboardingForm from "./components/Auth/OnboardingForm";
 export default function Home() {
+    //TODO Redirect to dashboard on sign in success
     return (
-        <main>
-            <h1>dashboard</h1>
+        <main className="py-4 px-4 md:px-6 lg:px-8 xl:px-10 min-h-screen grid place-items-center">
+            <OnboardingForm />
         </main>
     );
 }
