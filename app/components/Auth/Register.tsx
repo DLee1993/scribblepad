a2097c82dbdb7c71ee7c62c3df6201700645a0a4
David Lee <Dai180293@gmail.com>
2024-06-17 14:48:57 +0100

    added sign in and register components

---
+++ app/components/Auth/Register.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+
+export default function Register() {
+    //TODO Redirect to dashboard on sign in success
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="outline">Register</Button>
+            </SheetTrigger>
+            <SheetContent className="w-full sm:max-w-sm">
+                <SheetHeader>
+                    <SheetTitle>Create an account</SheetTitle>
+                    <SheetDescription>
+                        Make changes to your profile here. Click save when youre done.
+                    </SheetDescription>
+                </SheetHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Name
+                        </Label>
+                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Username
+                        </Label>
+                        <Input id="username" value="@peduarte" className="col-span-3" />
+                    </div>
+                </div>
+                <SheetFooter>
+                    <SheetClose asChild>
+                        <Button type="submit">Save changes</Button>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
+    );
+}
