9d04f8e2f8f5184d14b644c7520c67ba7f7a93bf
David Lee <Dai180293@gmail.com>
2024-06-17 14:50:51 +0100

    removed onboarding form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import OnboardingForm from "./components/Auth/OnboardingForm";
 export default function Home() {
-    //TODO Redirect to dashboard on sign in success
     return (
-        <main className="py-4 px-4 md:px-6 lg:px-8 xl:px-10 min-h-screen grid place-items-center">
-            <OnboardingForm />
+        <main>
+            landing page to showcase scribblepad
         </main>
     );
 }
