fa6e9a1562957671eb8fbe09a3fc23f88ebb02a4
David Lee <Dai180293@gmail.com>
2024-06-17 14:53:47 +0100

    removed hard coded links and replaced with a map of the pageLinks file

--- app/components/Navbar/Menu.tsx
+++ app/components/Navbar/Menu.tsx
@@ -12,14 +12,8 @@ import {
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet";
-import {
-    HamburgerMenuIcon,
-    ReaderIcon,
-    CalendarIcon,
-    CheckboxIcon,
-    DesktopIcon,
-    PersonIcon,
-} from "@radix-ui/react-icons";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { pageLinks } from "./pageLinks";
 
 export default function Menu() {
     const [menuStatus, setMenuStatus] = useState<boolean>(false);
@@ -39,78 +33,29 @@ export default function Menu() {
                 className="flex flex-col justify-start items-start py-6 px-3 sm:max-w-[18rem] capitalize dark:bg-[var(--background)]"
             >
                 <SheetHeader>
-                    <SheetTitle className="font-bold text-md pl-3">
-                        <Link href="/" onClick={changeMenuStatus}>
-                            Scribblepad
-                        </Link>
-                    </SheetTitle>
+                    <SheetTitle className="font-bold text-md pl-3">Scribblepad</SheetTitle>
                 </SheetHeader>
                 <section className="flex flex-col justify-between items-start flex-1 w-full mt-10 text-base">
                     <ul id="menuList">
-                        <li>
-                            <Link
-                                href="/dashboard"
-                                onClick={changeMenuStatus}
-                                className={`link ${pathname === "/dashboard" ? "active" : ""}`}
-                            >
-                                <span>
-                                    <DesktopIcon />
-                                </span>
-                                dashboard
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href="/notes"
-                                onClick={changeMenuStatus}
-                                className={`link ${pathname === "/notes" ? "active" : ""}`}
-                            >
-                                <span>
-                                    <ReaderIcon />
-                                </span>
-                                notes
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href="/tasks"
-                                onClick={changeMenuStatus}
-                                className={`link ${pathname === "/tasks" ? "active" : ""}`}
-                            >
-                                <span>
-                                    <CheckboxIcon />
-                                </span>
-                                tasks
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href="/calendar"
-                                onClick={changeMenuStatus}
-                                className={`link ${pathname === "/calendar" ? "active" : ""}`}
-                            >
-                                <span>
-                                    <CalendarIcon />
-                                </span>
-                                calendar
-                            </Link>
-                        </li>
+                        {pageLinks.map(({ href, pageName, icon }, i) => (
+                            <li key={i} id={`link-${i}`}>
+                                <Link
+                                    href={href}
+                                    onClick={changeMenuStatus}
+                                    className={`link ${
+                                        pathname === `/${pageName}` ? "active" : ""
+                                    }`}
+                                >
+                                    {pageName}
+                                    <span>{icon}</span>
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </section>
                 <Separator />
                 <SheetFooter className="w-full text-base" id="footer">
-                    <Link
-                        href="/support"
-                        onClick={changeMenuStatus}
-                        className={`w-full flex justify-start items-center gap-x-2 py-2 rounded-md pl-3 ${
-                            pathname === "/support" ? "active" : ""
-                        }`}
-                    >
-                        <span>
-                            <PersonIcon />
-                        </span>
-                        support
-                    </Link>
+                    add your socials here
                 </SheetFooter>
             </SheetContent>
         </Sheet>
