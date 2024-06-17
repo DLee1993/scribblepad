4fbb705c91cb5e815221faaf77b690f73871d477
David Lee <Dai180293@gmail.com>
2024-06-17 14:53:17 +0100

    added a temp conditional to render components based on current page

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,18 +1,28 @@
 "use client";
 import Menu from "./Menu";
 import Account from "./Account";
+import Register from "../Auth/Register";
 import { usePathname } from "next/navigation";
+import SignIn from "../Auth/SignIn";
 
 export default function Navbar() {
-    //TODO only show navbar based on session, if no session, no navbar
+    //TODO only show navbar content based on session, if no session, no navbar
     const pathname = usePathname();
     return (
         <>
-            {pathname !== "/" && (
+            {pathname !== "/" ? (
                 <nav className="flex justify-between items-center py-4 px-4 md:px-6 lg:px-8 xl:px-10 border-b-2 border-slate-950/10 dark:border-slate-50/10">
                     <Menu />
                     <section className="flex justify-center items-center gap-x-4">
                         <Account />
+                    </section>
+                </nav>
+            ) : (
+                <nav className="flex justify-between items-center py-4 px-4 md:px-6 lg:px-8 xl:px-10">
+                    <h1 className="text-md font-medium">Scribblepad</h1>
+                    <section className="flex justify-center items-center gap-x-4">
+                        <Register />
+                        <SignIn />
                     </section>
                 </nav>
             )}
