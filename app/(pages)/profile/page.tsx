12041b182edd64b84adee5046497f4b7229643af
David Lee <Dai180293@gmail.com>
2024-06-21 20:28:21 +0100

    added comments for related content

--- app/(pages)/profile/page.tsx
+++ app/(pages)/profile/page.tsx
@@ -1,5 +1,4 @@
+//? This page needs to be able to add custom tags and colors for a user to add to either tasks or notes. This will need to be a nested list within the users database?
 export default function page() {
-  return (
-    <div>profile</div>
-  )
+    return <div>profile</div>;
 }
